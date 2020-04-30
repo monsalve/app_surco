7141588f6036bcc5e01ee074c088e72d2418f5ec
Cristhiam Monsalve <ccmonapa@hotmail.com>
2020-04-30 01:33:59 -0500

    listo menu

--- resources/js/components/AppContainerComponent.vue
+++ resources/js/components/AppContainerComponent.vue
@@ -44,38 +44,16 @@
         <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark >
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
                 <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-                    <span class="hidden-sm-and-down">Google Contacts</span>
+                    <span class="hidden-sm-and-down">Surcoestudios</span>
                 </v-toolbar-title>
-            <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search" class="hidden-sm-and-down" />
+            
             <v-spacer />
-            <v-btn icon> <v-icon>mdi-apps</v-icon> </v-btn>
-            <v-btn icon> <v-icon>mdi-bell</v-icon> </v-btn>
-            <v-btn icon large>
-                <v-avatar size="32px" item >
-                    <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"/>
-                </v-avatar>
-            </v-btn>
+            <span class=".font-weight-bold">Cristiam Monsalve</span>            
+            <v-btn icon> <v-icon>mdi-bell</v-icon> </v-btn>            
         </v-app-bar>
         <v-content>
             <v-container class="fill-height" fluid>
-                <v-row align="center" justify="center">
-                    <v-tooltip right>
-                        <template v-slot:activator="{ on }">
-                            <v-btn :href="source" icon large target="_blank" v-on="on">
-                                <v-icon large>mdi-code-tags</v-icon>
-                            </v-btn>
-                        </template>
-                        <span>Source</span>
-                    </v-tooltip>
-                    <v-tooltip right>
-                        <template v-slot:activator="{ on }">
-                            <v-btn icon large href="https://codepen.io/johnjleider/pen/MNYLdL" target="_blank" v-on="on">
-                                <v-icon large>mdi-codepen</v-icon>
-                            </v-btn>
-                        </template>
-                        <span>Codepen</span>
-                    </v-tooltip>
-                </v-row>
+                xxxxxxx
             </v-container>
         </v-content>
         <v-btn bottom color="pink" dark fab fixed right @click="dialog = !dialog"><v-icon>mdi-plus</v-icon></v-btn>
@@ -117,19 +95,9 @@
             </v-dialog>
         </v-app>
     </template>
-<script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      dialog: false,
-      drawer: null,
-      items: [
-        { icon: 'mdi-contacts', text: 'Contacts' },
-        { icon: 'mdi-history', text: 'Frequently contacted' },
-        { icon: 'mdi-content-copy', text: 'Duplicates' },
-        {
+    <!--
+
+{
           icon: 'mdi-chevron-up',
           'icon-alt': 'mdi-chevron-down',
           text: 'Labels',
@@ -138,24 +106,26 @@
             { icon: 'mdi-plus', text: 'Create label' },
           ],
         },
-        {
-          icon: 'mdi-chevron-up',
-          'icon-alt': 'mdi-chevron-down',
-          text: 'More',
-          model: false,
-          children: [
-            { text: 'Import' },
-            { text: 'Export' },
-            { text: 'Print' },
-            { text: 'Undo changes' },
-            { text: 'Other contacts' },
-          ],
-        },
-        { icon: 'mdi-settings', text: 'Settings' },
-        { icon: 'mdi-message', text: 'Send feedback' },
-        { icon: 'mdi-help-circle', text: 'Help' },
-        { icon: 'mdi-cellphone-link', text: 'App downloads' },
-        { icon: 'mdi-keyboard', text: 'Go to the old version' },
+
+     -->
+<script>
+    
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      dialog: false,
+      drawer: null,
+      items: [
+        { icon: 'mdi-account-card-details', text: 'Estudiantes' },        
+        { icon: 'mdi-clipboard-text', text: 'Inscripciones' },
+        { icon: 'mdi-teach', text: 'Tutores' },
+        { icon: 'mdi-folder-multiple', text: 'Cursos' },
+        { icon: 'mdi-factory', text: 'Empresas' },        
+        { icon: 'mdi-account-circle', text: 'Mi cuenta' },        
+        { icon: 'mdi-contacts', text: 'Usuarios' },
+        { icon: 'mdi-settings', text: 'Configuraciones' },
         { icon: 'mdi-exit-to-app', text: 'Salir', click_salir:'alerta' },
       ],
     }),
